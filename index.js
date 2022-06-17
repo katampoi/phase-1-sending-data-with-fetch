d5c5c3bf738bdb9a83f229c0597b8c953f9f2079
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-17 14:19:18 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+
+const submitData= (name,email)=>{
+        method: "POST",
+        headers; {"Content-Type"; "application/json",
+                   "Accept"; "application/json"
+                }
+    body: JSON.stringify()
+    .then(response=> response.json())
+    .then(object=>document.body.innerHTML = object["id"])
+    .catch(error =>document.body.innerHTML =error.message)
+};
+    
+
+
+

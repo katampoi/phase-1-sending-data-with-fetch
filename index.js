edf6e9540661556268c2e605705cf9701d11ef8b
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-18 13:59:04 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 // Add your code here
 
-const submitData= (name,email)=>{
-        method: "POST",
-        headers; {"Content-Type"; "application/json",
-                   "Accept"; "application/json"
-                }
-    body: JSON.stringify()
-    .then(response=> response.json())
-    .then(object=>document.body.innerHTML = object["id"])
-    .catch(error =>document.body.innerHTML =error.message)
-};
-    
-
-
-
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
